8cb1f0b53fb6f09ab74d2af3be0b394bf93bef40
Papalarva <papalarva1@gmail.com>
2022-01-08 20:09:54 -0700

    Se modifico el estilo de oferta academica

---
+++ pages/layout/footer.php
@@ -0,0 +1,46 @@
+    <!-- FOOTER -->
+    <footer>
+        <section class="footer__contenedor">
+            <div class="footer__seccion footer__seccion-Ocultar">
+                <h1 class="footer__titulos">Sobre nosotros</h1>
+                <p class="footer__parrafo">
+                    Nuestro compromiso es seguir trabajando con el apoyo de toda la comunidad que integra el CBTIS 128 para lograr formar técnicos profesionales capaces de destacar en el 	ambito laboral. Desde hace más de 50 años hemos brindado las herramientras para que nuestros estudiantes logren sobresalir en la industria y sus vidas profesionales. Estamos comprometidos con la sociedad juarense y creemos firmemente en las educación y los valores que hemos impartido en cada uno de nuestros ingresados.
+                </p>
+                <a class="footer__rrss" target="_blank" href="https://www.facebook.com/Casa-de-los-Jaguares-CBTIS-128-101239285643821/">
+                    <i class='bx bxl-facebook footer__iconRRSS'></i>
+                </a>
+                <a class="footer__rrss" target="_blank" href="https://www.youtube.com/channel/UCBuXio9_FP2pdABkHykR67Q/videos">
+                    <i class='bx bxl-youtube footer__iconRRSS'></i>
+                </a>
+            </div>
+            <div class="footer__seccion">
+                <h1 class="footer__titulos">Avisos de Privacidad</h1>
+                <a class="footer__enlaces" target="_blank" href="assets/document/avisos/Aviso_de_Privacidad_Integral_Planteles-1.pdf">Aviso de Privacidad Integral Planteles</a>
+                <a class="footer__enlaces" target="_blank" href="assets/document/avisos/Aviso_de_Privacidad_Simplificado_Planteles.pdf">Aviso de Privacidad Simplificado Planteles</a>
+                <a class="footer__enlaces" target="_blank" href="assets/document/avisos/LPP.pdf">Política pública para la educación media superior</a>
+            </div>
+            <div class="footer__seccion">
+                <h1 class="footer__titulos">Información de Contacto</h1>
+                <ul>
+                    <li class="footer__contacto">
+                        <span><i class='bx bxs-map contacto__icon' ></i></span>
+                        <a class="footer__enlaces contacto__enlaces" target="_blank" href="https://goo.gl/maps/ATBLjX1t7thyiGRs5">CBTIS 128, Nueva Zelanda, Oasis</a>
+                    </li>
+                    <li class="footer__contacto">
+                        <span><i class='bx bxs-phone contacto__icon'></i></span>
+                        <div class="footer__phones">
+                            <a class="footer__enlaces contacto__enlaces" href="tel: 656-619-1934">656-619-1934</a> 
+                            <a class="footer__enlaces contacto__enlaces" href="tel: 656-619-1570">656-619-1570</a>
+                        </div>
+                    </li>
+                    <li class="footer__contacto">
+                        <span><i class='bx bxl-gmail contacto__icon' ></i></span>
+                        <a class="footer__enlaces contacto__enlaces" href="mailto: comite-escolar.128@gmail.com">comite-escolar.128@gmail.com</a>
+                    </li>
+                </ul>
+            </div>
+        </section>
+        <div class="seccion__copyright">
+            <p>Copyright © 2021 CBTIS 128. All Right Reserved</p>
+        </div>
+    </footer>
